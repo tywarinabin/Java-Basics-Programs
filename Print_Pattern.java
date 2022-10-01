25a35e3e5d10007e29440be3837eb593545ea7ab
Nabin Tiwari <96787565+tywarinabin@users.noreply.github.com>
2022-10-01 21:46:02 +0530

    Print pattern like Half Pyramid

---
+++ Print_Pattern.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+public class Print_Pattern {
+    public static void main(String args []){
+        Scanner sc = new Scanner(System.in);
+       System.out.println("Enter number : ");
+        int a = sc.nextInt();
+        for(int i = a ;i>=0;i--){
+            for(int j = 1;j<=i;j++){
+                System.out.print("These");
+            }
+            System.out.println(" ");
+        }
+        
+    }
+}
